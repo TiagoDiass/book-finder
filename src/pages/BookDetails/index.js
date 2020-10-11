9377ab6aab8d4aaf3b479ea1930717b915dcceee
TiagoDiass <tiago.costadiasss@gmail.com>
2020-10-11 11:23:06 -0300

    buy block done

--- src/pages/BookDetails/index.js
+++ src/pages/BookDetails/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
 import Loading from '../../components/Loading';
 
-import { FullPageContainer, Container, LeftBlock } from './styles';
+import { FullPageContainer, Container, LeftBlock, MiddleBlock, RightBlock } from './styles';
 
 function BookDetails(props) {
   const [book, setBook] = useState({});
@@ -18,9 +20,31 @@ function BookDetails(props) {
           ? response.volumeInfo.imageLinks.thumbnail
           : 'https://books.google.com.br/googlebooks/images/no_cover_thumb.gif';
 
+        let saleInfo;
+
+        if (response.saleInfo.saleability === 'FOR_SALE') {
+          saleInfo = {
+            isForSale: true,
+            price: response.saleInfo.listPrice.amount,
+            currency: response.saleInfo.listPrice.currencyCode,
+            priceFormatted: new Intl.NumberFormat(
+              response.saleInfo.listPrice.currencyCode === 'BRL' ? 'pt-BR' : 'en-US',
+              {
+                style: 'currency',
+                currency: response.saleInfo.listPrice.currencyCode,
+              }
+            ).format(response.saleInfo.listPrice.amount),
+          };
+        } else {
+          saleInfo = {
+            isForSale: false,
+          };
+        }
+
         const bookLoaded = {
           id: response.id,
           title: response.volumeInfo.title,
+          subtitle: response.volumeInfo.subtitle,
           authors: response.volumeInfo.authors,
           description: response.volumeInfo.description,
           thumbnailUrl,
@@ -29,15 +53,18 @@ function BookDetails(props) {
           averageRating: response.volumeInfo.averageRating,
           publisher: response.volumeInfo.publisher,
           publishedDate: response.volumeInfo.publishedDate,
+          categories: response.volumeInfo.categories ? response.volumeInfo.categories[0].split(' / ') : [],
+          saleInfo,
         };
 
         setBook(bookLoaded);
+
         setLoading(false);
       });
     }
 
     fetchBook();
-  }, []);
+  }, [props.match.params.book_id]);
 
   return (
     <>
@@ -45,6 +72,9 @@ function BookDetails(props) {
         <Loading />
       ) : (
         <FullPageContainer>
+          <Link to="/" className="back-link">
+            <i className="fas fa-arrow-left"></i> Back
+          </Link>
           <Container>
             <LeftBlock className="book-image">
               <div className="title-and-image">
@@ -65,9 +95,46 @@ function BookDetails(props) {
                 <div className="stat-row">
                   <label>Pages amount:</label> <span>{book.pagesAmount}</span>
                 </div>
-                {/* <div dangerouslySetInnerHTML={{ __html: book.description }}></div> */}
+                <div className="stat-row">
+                  <label>Average Rating:</label> <span>{book.averageRating}</span>
+                </div>
               </div>
             </LeftBlock>
+            <MiddleBlock cover2Columns={book.saleInfo && book.saleInfo.isForSale}>
+              <div className="desc-row">
+                <h2>Description</h2>
+                <p className="description" dangerouslySetInnerHTML={{ __html: book.description }}></p>
+              </div>
+            </MiddleBlock>
+            {book.saleInfo && book.saleInfo.isForSale && (
+              <RightBlock>
+                <div className="is-for-sale-label">
+                  FOR SALE <i className="fas fa-check"></i>
+                </div>
+
+                <div className="book-mini-data-row">
+                  <h2>{book.title}</h2>
+                  <h4>{book.subtitle}</h4>
+                  <div className="categories">
+                    {book.categories.map((category, index) => (
+                      <span key={index}>{category}</span>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="price-row">
+                  <span className="pre-price-label">You can buy this book at Google Books for</span>
+                  <span className="price">{book.saleInfo.priceFormatted}</span>
+                </div>
+
+                <div className="button-row">
+                  <a href={book.infoLink} target="_blank" rel="noopener noreferrer">
+                    BUY THIS BOOK
+                    <i className="fas fa-external-link-alt"></i>
+                  </a>
+                </div>
+              </RightBlock>
+            )}
           </Container>
         </FullPageContainer>
       )}
--- src/pages/BookDetails/styles.js
+++ src/pages/BookDetails/styles.js
@@ -7,6 +7,25 @@ export const FullPageContainer = styled.div`
   justify-content: center;
   align-items: center;
   background-color: var(--white-darker);
+
+  a.back-link {
+    color: var(--color-blue);
+    text-decoration: none;
+    position: absolute;
+    top: 1.1rem;
+    left: 1.1rem;
+    font-size: 1.1rem;
+    display: flex;
+    align-items: center;
+
+    i {
+      margin-right: 6px;
+    }
+
+    &:hover {
+      color: var(--color-blue-darker);
+    }
+  }
 `;
 
 export const Container = styled.div`
@@ -14,14 +33,14 @@ export const Container = styled.div`
   height: 65%;
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 export const LeftBlock = styled.aside`
   background-color: var(--white);
-  width: 30%;
-  height: 100%;
   padding: 1rem;
+  width: 25%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -59,6 +78,152 @@ export const LeftBlock = styled.aside`
 
       span {
         font-weight: 400;
+      }
+    }
+  }
+`;
+
+export const MiddleBlock = styled.div`
+  height: 100%;
+  width: 70%;
+
+  ${(props) =>
+    props.cover2Columns &&
+    `
+    width: 45%;
+  `}
+
+  border-radius: 0.5rem;
+  box-shadow: 0 0 8px 3px rgba(0, 0, 0, 0.15);
+  background-color: var(--white);
+  padding: 1rem 1.2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 6px;
+  overflow-y: scroll;
+  /* width */
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  /* Track */
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+  }
+
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: var(--color-primary);
+    border-radius: 10px;
+  }
+
+  /* Handle on hover */
+  &::-webkit-scrollbar-thumb:hover {
+    filter: brightness(0.9);
+  }
+
+  h2 {
+    font-weight: 500;
+    margin-bottom: 0.9rem;
+  }
+
+  p.description {
+    font-weight: 400;
+  }
+`;
+
+export const RightBlock = styled.div`
+  width: 25%;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 8px 3px rgba(0, 0, 0, 0.15);
+  background-color: var(--white);
+  padding: 1rem;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .is-for-sale-label {
+    text-align: center;
+    font-size: 1.6rem;
+    color: var(--color-blue);
+  }
+
+  .book-mini-data-row {
+    h2 {
+      font-size: 1.4rem;
+      font-weight: 500;
+      font-family: 'Space Mono', monospace;
+      text-align: center;
+    }
+
+    h4 {
+      font-size: 1.1rem;
+      font-weight: 300;
+      font-family: 'Space Mono', monospace;
+      text-align: center;
+      margin-bottom: 0.7rem;
+    }
+
+    div.categories {
+      display: flex;
+      justify-content: space-around;
+      flex-wrap: wrap;
+
+      span {
+        display: inline-block;
+        padding: 0.2rem 0.5rem;
+        background-color: var(--color-primary);
+        color: var(--white);
+        border-radius: 0.25rem;
+        margin: 0.2rem 0;
+      }
+    }
+  }
+
+  .price-row {
+    display: flex;
+    flex-direction: column;
+
+    span.pre-price-label {
+      color: var(--color-primary);
+      font-size: 1.55rem;
+    }
+
+    span.price {
+      font-size: 2.4rem;
+      color: var(--color-blue);
+      font-weight: 500;
+      letter-spacing: 1px;
+    }
+  }
+
+  .button-row {
+    a {
+      display: block;
+      text-decoration: none;
+      text-align: center;
+      padding: 0.8rem;
+      width: 100%;
+      border: 2px solid var(--color-blue);
+      outline: none;
+      background: transparent;
+      border-radius: 0.3rem;
+      color: var(--color-blue);
+      font-size: 0.95rem;
+      cursor: pointer;
+
+      transition: background 0.2s ease, color 0.2s ease;
+
+      i {
+        margin-left: 8px;
+      }
+
+      &:hover {
+        background-color: var(--color-blue);
+        color: var(--white);
       }
     }
   }
